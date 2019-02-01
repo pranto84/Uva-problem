3b97515a3fa5afe0e325cbce579327f6e212d429
pranto saha <sahapranto32@gmail.com>
2019-02-02 02:14:38 +0600

    Hay Points

---
+++ uva 10295.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define sci(a)              scanf("%d", &a)
+#define sci2(a,b)           scanf("%d%d", &a, &b)
+#define sci3(a, b, c)       scanf("%d%d%d", &a, &b, &c)
+#define scll(a)             scanf("%lld",&a)
+#define scll2(a,b)          scanf("%lld%lld",&a,&b)
+#define scll3(a,b,c)        scanf("%lld%lld%lld",&a,&b,&c)
+#define scd(a)              scanf("%lf", &a)
+#define scd2(a,b)           scanf("%lf%lf",&a,&b)
+#define scd3(a,b,c)       scanf("%lf%lf%lf", &a, &b, &c)
+
+#define     gcd(a,b)  __gcd(a,b)
+#define     pi      2.0*acos(0.0)
+#define     pb      push_back
+#define     mp      make_pair
+#define     mod     998244353
+#define     pii     pair<int, int>
+#define     ff      first
+#define     ss      second
+#define     MAX     INT_MAX
+#define     MIN     INT_MIN
+#define     N       100005
+
+#define input(); freopen("input.txt", "r", stdin);
+#define output(); freopen("output.txt", "w", stdout);
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long ll;
+int main()
+{
+    int n, k, value;
+    map<string,int> M;
+    string str;
+    sci2(n,k);
+    while(n--){
+        cin >> str >> value;
+        M[str] = value;
+    }
+    while(k--){
+        int ans = 0;
+        while(cin >> str && str!="."){
+            ans += M[str];
+        }
+        cout << ans << endl;
+    }
+}
+
+
